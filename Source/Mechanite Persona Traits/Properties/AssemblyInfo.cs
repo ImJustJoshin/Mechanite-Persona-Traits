79513b3bef13426fc1d780d153ee9b62ae1ea88f
Joshua <joshuag915@gmail.com>
2023-01-19 15:21:49 -0500

    Added some actual assembly info if you actually read these.

--- Source/Mechanite Persona Traits/Properties/AssemblyInfo.cs
+++ Source/Mechanite Persona Traits/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Mechanite Persona Traits")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Created assembly for my mod, Mechanite Persona Traits. The union of a weaponized plague and Persona Traits.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Mechanite Persona Traits")]
+[assembly: AssemblyCompany("ImJustJoshin - Just a guy :)")]
+[assembly: AssemblyProduct("Rimworld Mod - Mechanite Persona Traits")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.19.2023.1")]
+[assembly: AssemblyFileVersion("1.19.2023.1")]
