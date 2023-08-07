7ae5ed7b7eac59e9169281336c26a559a68338b6
Joshua <joshuag915@gmail.com>
2023-08-07 19:10:39 -0400

    (WIP) Harmony Patch to fix description issues INCOMPLETE

---
+++ Source/Mechanite Persona Traits/Patches/MechaniteUIPatch.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using HarmonyLib;
+using MorePersonaTraits.Patches;
+using MechanitePersonaTraits.OnHitWorkerClasses;
+using RimWorld;
+using Verse;
+
+namespace MechanitePersonaTraits.Patches
+{
+    [StaticConstructorOnStartup]
+    public static class MechaniteUIPatch
+    {
+        static MechaniteUIPatch()
+        {
+            //Fires Second
+            Log.Message("Static Mechanite Persona Traits Mod Class Loaded");
+
+            Harmony harmony = new Harmony("rimworld.mod.ImJustJoshin.MechanitePersonaTraits");
+            harmony.PatchAll();
+        }
+    }
+    
+    [HarmonyPatch]
+    public static class MechaniteCompBladelinkWeaponPatches
+    {
+        [HarmonyPatch(typeof(CompBladelinkWeapon), nameof(PatchWeaponTraitDefSpecialDisplayStats))]
+        [HarmonyPostfix]
+        public static void Mechanite_Postfix()
+        {
+
+        }
+    }
+}
