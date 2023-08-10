b11d2e085310f4fa921d45cf15ed47c4d596c78b
Joshua <joshuag915@gmail.com>
2023-08-10 16:23:24 -0400

    Complete Prefix Patches!

--- Source/Mechanite Persona Traits/Patches/MechaniteUIPatch.cs
+++ Source/Mechanite Persona Traits/Patches/MechaniteUIPatch.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using HarmonyLib;
-using MorePersonaTraits.Extensions;
+using MorePersonaTraits.Patches;
 using MechanitePersonaTraits.OnHitWorkerClasses;
 using MorePersonaTraits.WorkerClasses.OnHitWorkerClasses;
 using RimWorld;
@@ -10,129 +10,59 @@ using Verse;
 namespace MechanitePersonaTraits.Patches
 {
     //PatchWeaponTraitDefSpecialDisplayStats made by Arquebus. Modified by ImJustJoshin.
-
-    [HarmonyPatch(typeof(Def), "SpecialDisplayStats")]
-    public static class MechanitePatchWeaponTraitDefSpecialDisplayStats
+    [HarmonyPatch(typeof(PatchWeaponTraitDefSpecialDisplayStats), methodName: "ReportText")]
+    static class ReportTextPatch
     {
-        //Nothing was touched here.
-        static IEnumerable<StatDrawEntry> Postfix(IEnumerable<StatDrawEntry> values, Def __instance)
-        {
-            var statEntries = new List<StatDrawEntry>();
-            if (__instance is WeaponTraitDef traitDef)
-            {
-                if (traitDef.equippedStatOffsets != null)
-                {
-                    foreach (var equippedOffset in traitDef.equippedStatOffsets)
-                    {
-                        statEntries.Add(
-                            new StatDrawEntry(
-                                StatCategoryDefOf.EquippedStatOffsets,
-                                equippedOffset.stat.label,
-                                equippedOffset.ValueToStringAsOffset,
-                                equippedOffset.stat.description,
-                                6010
-                            )
-                        );
-                    }
-                }
-
-                if (traitDef.GetModExtension<WeaponTraitOnHitExtension>() != null && traitDef.GetModExtension<WeaponTraitOnHitExtension>().OnHitWorkers != null)
-                {
-                    foreach (var worker in traitDef.GetModExtension<WeaponTraitOnHitExtension>().OnHitWorkers)
-                    {
-                        statEntries.Add(new StatDrawEntry(
-                            MPT_StatCategoryDefOf.MPT_OnHitEffects,
-                            workerLabel(worker),
-                            ReportText(worker),
-                            ReportText(worker),
-                            6010
-                        ));
-                    }
-                }
-
-                // if (traitDef.exclusionTags != null)
-                // {
-                //     statEntries.Add(new StatDrawEntry(
-                //         StatCategoryDefOf.Weapon,
-                //         "ExclusionTags",
-                //         traitDef.exclusionTags.ToCommaList(),
-                //         "List of exclusion tags:\n" + traitDef.exclusionTags.ToCommaList(),
-                //         6010
-                //     ));
-                // }
-
-                if (traitDef.bondedHediffs != null)
-                {
-                    foreach (var hediff in traitDef.bondedHediffs)
-                    {
-                        statEntries.AddRange(hediff.SpecialDisplayStats(StatRequest.ForEmpty()));
-                    }
-                }
-            }
-
-            statEntries.AddRange(values);
-            return statEntries;
-        }
-        private static TaggedString ReportText(OnHitWorker worker)
+        private static bool Prefix(OnHitWorker worker, ref TaggedString __result)
         {
             //Newly added Switch case to detect ApplyMechanite.
             //If not then default to original Arquebus code.
             switch (worker)
             {
                 case OnHitWorker_ApplyMechanite onHit:
-                    return "MPT_MechaniteOnHitDesc".Translate(
+                    __result = "MPT_MechaniteOnHitDesc".Translate(
                     worker.ProcChance.ToStringPercent(),
-                    workerEffect(worker),
+                    "MPT_ApplyMechaniteDesc".Translate("Mechanite plague"),
                     worker.TargetSelf ? "MPT_TargetSelf".Translate() : "MPT_TargetTarget".Translate(),
                     Math.Abs(onHit.minInfectionSeverity) > 0f ? "MPT_MinSeverityDesc".Translate((onHit.minInfectionSeverity * 100).ToString()) : TaggedString.Empty,
                     Math.Abs(onHit.maxInfectionSeverity) > 0f ? "MPT_MaxSeverityDesc".Translate(onHit.maxInfectionSeverity.ToStringPercent()) : TaggedString.Empty,
                     onHit.extraSpawns > 0 ? "\n\n" + "MPT_ExtraSpawnDesc".Translate(onHit.extraSpawns.ToString()) : TaggedString.Empty
                     );
+                    return false;
                 default:
-                    return "MPT_OnHitDesc".Translate(
-                    worker.ProcChance.ToStringPercent(),
-                    workerEffect(worker),
-                    worker.TargetSelf ? "MPT_TargetSelf".Translate() : "MPT_TargetTarget".Translate(),
-                    Math.Abs(worker.ProcMagnitude) > 0f ? "MPT_MagnitudeDesc".Translate(worker.ProcMagnitude.ToStringPercent()) : TaggedString.Empty
-                    );
+                    return true;
             }
         }
-        private static string workerEffect(OnHitWorker worker)
+    }
+    [HarmonyPatch(typeof(PatchWeaponTraitDefSpecialDisplayStats), methodName: "workerEffect")]
+    static class workerEffectPatch
+    {
+        private static bool Prefix(OnHitWorker worker, ref string __result)
         {
             //Added one new case for ApplyMechanite
             switch (worker)
             {
                 case OnHitWorker_ApplyMechanite onHit:
-                    return "MPT_ApplyMechaniteDesc".Translate("Mechanite plague");
-                case OnHitWorker_ApplyHediff onHit:
-                    return "MPT_ApplyHediffDesc".Translate(onHit.HediffDef.LabelCap);
-                case OnHitWorker_ApplyGeneResource onHit:
-                    return "MPT_ApplyGeneResourceDesc".Translate(onHit.GeneDef.resourceLabel);
-                case OnHitWorker_ApplyNeed onHit:
-                    return onHit.ProcMagnitude > 0f ? "MPT_NeedOffsetTypeUpDesc".Translate(onHit.NeedDef.LabelCap) : "MPT_NeedOffsetTypeDownDesc".Translate(onHit.NeedDef.LabelCap);
-                case OnHitWorker_ApplyThought onHit:
-                    return "MPT_GiveThoughDesc".Translate(onHit.ThoughtDef.Label, onHit.ThoughtDef.stackLimit > 1 ? "MPT_ThoughtStackDesc".Translate(onHit.ThoughtDef.stackLimit) : TaggedString.Empty);
+                    __result = "MPT_ApplyMechaniteDesc".Translate("Mechanite plague");
+                    return false;
                 default:
-                    return "";
+                    return true;
             }
         }
-        private static string workerLabel(OnHitWorker worker)
+    }
+    [HarmonyPatch(typeof(PatchWeaponTraitDefSpecialDisplayStats), methodName: "workerLabel")]
+    static class workerLabelPatch
+    {
+        private static bool Prefix(OnHitWorker worker, ref string __result)
         {
             //Added one new case for ApplyMechanite
             switch (worker)
             {
                 case OnHitWorker_ApplyMechanite onHit:
-                    return "MPT_HediffDef".Translate();
-                case OnHitWorker_ApplyHediff onHit:
-                    return onHit.HediffDef.LabelCap;
-                case OnHitWorker_ApplyGeneResource onHit:
-                    return onHit.GeneDef.resourceLabel;
-                case OnHitWorker_ApplyNeed onHit:
-                    return onHit.NeedDef.LabelCap;
-                case OnHitWorker_ApplyThought onHit:
-                    return onHit.ThoughtDef.LabelCap;
+                    __result = "MPT_HediffDef".Translate();
+                    return false;
                 default:
-                    return "";
+                    return true;
             }
         }
     }
@@ -145,7 +75,7 @@ namespace MechanitePersonaTraits.Patches
             //Fires Second
             Log.Message("Static Mechanite Persona Traits Mod Class Loaded");
 
-            Harmony harmony = new Harmony("rimworld.mod.ImJustJoshin.MechanitePersonaTraits");
+            var harmony = new Harmony("rimworld.mod.ImJustJoshin.MechanitePersonaTraits");
             harmony.PatchAll();
         }
     }
